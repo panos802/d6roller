ff88ebb08dfeca4caa9b4439eda152b038f37d67
Παναγιώτης Αλέξανδρος Φραγκούλης <panos802@gmail.com>
2025-01-28 01:07:53 +0200

    cache all

--- service_worker.js
+++ service_worker.js
@@ -1,17 +1,26 @@
 // Cached core static resources
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("static").then((cache) => {
-      return cache.addAll(["./", "./images/logo192.png"]);
-    })
-  );
+	e.waitUntil(
+		caches.open("static").then((cache) => {
+			return cache.addAll([
+				"./",
+				"./index.css",
+				"./index.js",
+				"./favicon.ico",
+				"./assets/logo192.png",
+				"./assets/logo512.png",
+				"./assets/screenshot1.png",
+				"./assets/screenshot2.png",
+			]);
+		}),
+	);
 });
 
 // Fatch resources
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+	e.respondWith(
+		caches.match(e.request).then((response) => {
+			return response || fetch(e.request);
+		}),
+	);
 });
